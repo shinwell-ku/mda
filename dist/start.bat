4566c9de26a19a6a30403f6133e09a26879e8515
shinwell-ku <31087516+shinwell-ku@users.noreply.github.com>
2025-12-09 15:20:09 +0800

    Delete dist directory

--- dist/start.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SETLOCAL ENABLEDELAYEDEXPANSION
-SET classpath=.
-FOR %%i IN (lib/*.*) DO (
-    SET classpath=!classpath!;lib/%%i
-)
-SET mainClass=com.raysdata.mda.gui.MdaAppWindow
-jre\bin\java -classpath %classpath% %mainClass%
-endlocal
